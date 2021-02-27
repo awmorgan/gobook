30c88c58a1e455ed7ad11d2fc4e62c74e3f4e317
Bill Morgan <arthurwilliammorgan@gmail.com>
2021-02-27 17:26:07 -0600

    treesort

---
+++ ch4/treesort/treesort.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := []int{4, 4, 2, 4, 5, 2, 6, 9, 100, 12, 17, 43, 32, 48, 20, 10, 39, 0, -1, 16}
+	fmt.Printf("%#v\n", x)
+	Sort(x)
+	fmt.Printf("%#v\n", x)
+}
+
+type tree struct {
+	value       int
+	left, right *tree
+}
+
+// Sort sorts values in place.
+func Sort(values []int) {
+	var root *tree
+	for _, v := range values {
+		root = add(root, v)
+	}
+	appendValues(values[:0], root)
+}
+
+// appendValues appends the elements of t to values in order
+// and returns the resulting slice.
+func appendValues(values []int, t *tree) []int {
+	if t != nil {
+		values = appendValues(values, t.left)
+		values = append(values, t.value)
+		values = appendValues(values, t.right)
+	}
+	return values
+}
+
+func add(t *tree, value int) *tree {
+	if t == nil {
+		// Equivalent to return &tree{value:value}.
+		t = new(tree)
+		t.value = value
+		return t
+	}
+	if value < t.value {
+		t.left = add(t.left, value)
+	} else {
+		t.right = add(t.right, value)
+	}
+	return t
+}
