3b2e3ea60052312552e28c4cdda385fb88fe461f
Bill Morgan <arthurwilliammorgan@gmail.com>
2021-03-02 19:03:01 -0600

    fetch to file

--- ch5/fetch/fetch.go
+++ ch5/fetch/fetch.go
@@ -5,28 +5,45 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"path"
 	"time"
 )
 
 func main() {
 	start := time.Now()
 	for _, url := range os.Args[1:] {
-		start1 := time.Now()
-		resp, err := http.Get(url)
+		filename, b, err := fetch( url )
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
-			os.Exit(1)
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err )
+			continue
 		}
-		// _, err = io.ReadAll(resp.Body)
-		nbytes, err := io.Copy(io.Discard, resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
-			os.Exit(1)
-		}
-		// fmt.Printf("%s", b)
-		fmt.Printf("%.2fs  %7d  %s\n", time.Since(start1).Seconds(), nbytes, url)
-		
+		fmt.Printf("%s %d\n", filename, b)
 	}
 	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+// Fetch downloads the URL and returns the
+// name and length of the local file.
+func fetch(url string) (filename string, n int64, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", 0, err
+	}
+	defer resp.Body.Close()
+
+	local := path.Base(resp.Request.URL.Path)
+	if local == "/" {
+		local = "index.html"
+	}
+	f, err := os.Create(local)
+	if err != nil {
+		return "", 0, err
+	}
+
+	n, err = io.Copy(f, resp.Body)
+	// Close file, but prefer error from Copy, if any.
+	if closeErr := f.Close(); err == nil {
+		err = closeErr
+	}
+	return local, n, err
 }
