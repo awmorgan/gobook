ef1b459d23d99ffd156e4e38f30949b346ccae82
Bill Morgan <arthurwilliammorgan@gmail.com>
2021-03-05 11:26:48 -0600

    add more geometry examples

--- examples/geometry1/main.go
+++ examples/geometry1/main.go
@@ -10,4 +10,19 @@ func main() {
 	perim := geometry.Path{{1, 1}, {5, 1}, {5, 4}, {1, 1}}
 	fmt.Println(geometry.PathDistance(perim))
 	fmt.Println(perim.Distance())
+
+	r := &geometry.Point{1,2}
+	r.ScaleBy(2)
+	fmt.Printf("%#v\n", r)
+
+	p := geometry.Point{1,2}
+	pptr := &p
+	pptr.ScaleBy(2)
+	fmt.Println(p)
+
+	p = geometry.Point{1,2}
+	(&p).ScaleBy(2)
+	fmt.Println(p)
+	
+
 }
