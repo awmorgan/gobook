51db1c956b166579319f0fa16fb3d262ab825b63
Bill Morgan <arthurwilliammorgan@gmail.com>
2021-03-21 15:08:01 -0500

    table drivin test

--- ch11/word2/word_test.go
+++ ch11/word2/word_test.go
@@ -2,30 +2,31 @@ package word
 
 import "testing"
 
-func TestPalindrome(t *testing.T) {
-	if !IsPalindrome("detartrated") {
-		t.Error(`IsPalindrome("detartrated") = false`)
+func TestIsPalindrome(t *testing.T) {
+	tests := []struct {
+		name string
+		s    string
+		want bool
+	}{
+		{"empty string", "", true},
+		{"single letter", "a", true},
+		{"two same letters", "aa", true},
+		{"two different letters", "ab", false},
+		{"word1", "kayak", true},
+		{"word2", "detartrated", true},
+		{"sentence1", "A man, a plan, a canal: Panama", true},
+		{"sentence2", "Evil I did dwell; lewd did I live.", true},
+		{"sentence3", "Able was I ere I saw Elba", true},
+		{"French character", "été", true},
+		{"Foreign language", "Et se resservir, ivresse reste.", true},
+		{"Non-palindrome", "palindrome", false},
+		{"Semi-palindrome", "desserts", false},
 	}
-	if !IsPalindrome("kayak") {
-		t.Error(`IsPalindrome("kayak") = false`)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsPalindrome(tt.s); got != tt.want {
+				t.Errorf("IsPalindrome(%q) = %v, want %v", tt.s, got, tt.want)
+			}
+		})
 	}
 }
-
-func TestNonPalindrome(t *testing.T) {
-	if IsPalindrome("palindrome") {
-		t.Error(`IsPalindrome("palindrome") = true`)
-	}
-}
-
-func TestFrenchPalindrome(t *testing.T) {
-	if !IsPalindrome("été") {
-		t.Error(`IsPalindrome("été") = false`)
-	}
-}
-func TestCanalPalindrome(t *testing.T) {
-	input := "A man, a plan, a canal: Panama"
-	if !IsPalindrome(input) {
-		t.Errorf(`IsPalindrome(%q) = false`, input)
-	}
-}
-
