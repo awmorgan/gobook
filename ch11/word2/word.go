6f95db0d8165cf06a234d15cd9f6221adb7453e3
Bill Morgan <arthurwilliammorgan@gmail.com>
2021-03-24 19:00:30 -0500

    benchmark

--- ch11/word2/word.go
+++ ch11/word2/word.go
@@ -6,16 +6,24 @@ import "unicode"
 // IsPalindrome reports whether s reads teh same forward and backward.
 // (Our first attempt.)
 func IsPalindrome(s string) bool {
-	var letters []rune
+	// var letters []rune
+	letters := make([]rune, 0, len(s))
 	for _, r := range s {
 		if unicode.IsLetter(r) {
 			letters = append(letters, unicode.ToLower(r))
 		}
 	}
-	for i := range letters {
+
+	n := len(letters) / 2
+	for i := 0; i < n; i++ {
 		if letters[i] != letters[len(letters)-1-i] {
 			return false
 		}
 	}
+	// for i := range letters {
+	// 	if letters[i] != letters[len(letters)-1-i] {
+	// 		return false
+	// 	}
+	// }
 	return true
 }
