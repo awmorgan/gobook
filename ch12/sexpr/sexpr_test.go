d02f0db435c14e448114fbbcacc895194b00b4c5
Bill Morgan <arthurwilliammorgan@gmail.com>
2021-03-27 09:37:44 -0500

    unmarshal and test also fix bug

--- ch12/sexpr/sexpr_test.go
+++ ch12/sexpr/sexpr_test.go
@@ -1,22 +1,22 @@
 package sexpr_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/awmorgan/gobook/ch12/sexpr"
+	"github.com/google/go-cmp/cmp"
 )
 
 type Movie struct {
 	Title, Subtitle string
 	Year            int
 	// Color           bool
-	Actor           map[string]string
-	Oscars          []string
-	Sequel          *string
+	Actor  map[string]string
+	Oscars []string
+	Sequel *string
 }
 
-func TestMarshal(t *testing.T) {
+func TestMarshalUnmarshal(t *testing.T) {
 	strangelove := Movie{
 		Title:    "Dr. Strangelove",
 		Subtitle: "How I Learned to Stop Worrying and Love the Bomb",
@@ -41,5 +41,11 @@ func TestMarshal(t *testing.T) {
 	if err != nil {
 		t.Errorf("Marshal failed: %v", err)
 	}
-	fmt.Print(string(b))
+	var m Movie
+	if err := sexpr.Unmarshal(b, &m); err != nil {
+		t.Errorf("Unmarshal failed: %v", err)
+	}
+	if !cmp.Equal(m, strangelove) {
+		t.Errorf("m != strangelove: %s", cmp.Diff(m, strangelove))
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/awmorgan/gobook
 
 go 1.16
 
-require golang.org/x/net v0.0.0-20210226172049-e18ecbb05110
+require (
+	github.com/google/go-cmp v0.5.5
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110
+)
